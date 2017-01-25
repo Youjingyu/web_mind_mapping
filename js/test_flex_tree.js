84ab68290e8c7b605f4bc4813fa7cfa8a05114b7
Youjingyu <735284268@qq.com>
2017-01-25 17:37:41 +0800

    add modal logic

--- js/test_flex_tree.js
+++ js/test_flex_tree.js
@@ -35,15 +35,20 @@ d3.json('flextree.json', function (err, tree) {
         }
     }
 
+    var $iframe_doc = document.getElementById('html_code').contentWindow.document;
     // modal event
     var $modal = d3.select('#modal'),
-        $modal_cell = $modal.select('.modal-cell');
+        $modal_cell = $modal.select('.modal-cell'),
+        $modal_pre = $modal.select('.modal-code'),
+        $modal_code = $modal_pre.select('code');
     $modal.on("click.mask", function(){
-        if(d3.event.target.nodeName === 'IMG'){
+        var target_name = d3.event.target.nodeName;
+        if(target_name === 'IMG' || target_name ==='PRE'){
             return false;
         }
-        $modal.attr('style', 'display: none');
-        $modal.selectAll('img').attr('style', 'display: none');
+        $modal.style('display', 'none');
+        $modal.selectAll('img').style('display', 'none');
+        $modal_pre.style('display', 'none')
     });
 
     var client_width = document.documentElement.clientWidth,
@@ -168,7 +173,11 @@ d3.json('flextree.json', function (err, tree) {
         node.transition()
             .duration(duration)
             .attr("transform", function (d) {
-                d.y = d.depth * 180;
+                if(d.parent && d.parent.y0 && d.parent.y_size){
+                    d.y = d.parent.y0 + d.parent.y_size + 100;
+                } else {
+                    d.y = d.depth * 180;
+                }
                 return "translate(" + svg_x(d.y) + "," + (svg_y(d.x)-(d.x_size * scale - nodebox_vertical_margin) / 2) + ")";
             });
         nodeEnter.append("rect")
@@ -282,7 +291,7 @@ d3.json('flextree.json', function (err, tree) {
                 var o = {
                     x: source.x,
                     y: source.y,
-                    y_size: d.source.y_size
+                    y_size: source.y_size
                 };
                 return diagonal({
                     source: o,
@@ -321,33 +330,46 @@ d3.json('flextree.json', function (err, tree) {
     function parseText(text_tag, d){
         var $text = d3.select(text_tag),
             content = d.content;
-        if(/^(\.\/)?img\//.test(content)){
-            var img_id = content.replace(/[\/\.]/g, '');
-            if($modal_cell.select('#' + img_id)[0][0] === null){
-                $modal_cell.append('img').attr('id', img_id).attr('src', content).attr('style', 'display:none');
+        if(typeof d.content === 'string'){
+            if(/^(\.\/)?img\//.test(content)){
+                var img_id = content.replace(/[\/\.]/g, '');
+                if($modal_cell.select('#' + img_id)[0][0] === null){
+                    $modal_cell.append('img').attr('id', img_id).attr('src', content).attr('style', 'display:none');
+                }
+                $text.append('tspan').attr({x: '2', dy: '1.5em', path: content}).text('点击查看图片');
+                d3.select(text_tag.parentNode).attr('img_id', img_id).on("click.show", function(){
+                    $modal.select('#' + d3.select(this).attr('img_id')).attr("style", "display:inline-block");
+                    $modal.attr("style", "display: block");
+                });
+            } else {
+                var len = 0, split_str = '', reg = /[^\x00-\xff]/;
+                var split_arr = content.split(''),
+                    split_arr_len = split_arr.length - 1;
+                split_arr.forEach(function(val, i){
+                    if(reg.test(val)){
+                        len += 2;
+                    } else {
+                        len += 1;
+                    }
+                    split_str += val;
+                    if(len >= 30 || i >= split_arr_len){
+                        $text.append('tspan').attr({x: '2', dy: '1.5em'}).text(split_str);
+                        len = 0;
+                        split_str = '';
+                    }
+                });
             }
-            $text.append('tspan').attr({x: '2', dy: '1.5em', path: content}).text('点击查看图片');
-            d3.select(text_tag.parentNode).attr('img_id', img_id).on("click.show", function(){
-                            $modal.select('#' + d3.select(this).attr('img_id')).attr("style", "display:inline-block");
-                            $modal.attr("style", "display: block");
-                        });
         } else {
-            var len = 0, split_str = '', reg = /[^\x00-\xff]/;
-            var split_arr = content.split(''),
-                split_arr_len = split_arr.length - 1;
-            split_arr.forEach(function(val, i){
-                if(reg.test(val)){
-                    len += 2;
-                } else {
-                    len += 1;
-                }
-                split_str += val;
-                if(len >= 30 || i >= split_arr_len){
-                    $text.append('tspan').attr({x: '2', dy: '1.5em'}).text(split_str);
-                    len = 0;
-                    split_str = '';
-                }
-            });
+            if(d.content.type === 'html_code'){
+                $text.append('tspan').attr({x: '2', dy: '1.5em'}).text('点击查看代码');
+                d3.select(text_tag.parentNode).attr('code_id', d.content.id).on("click.show", function(){
+                    var code = $iframe_doc.getElementById(d3.select(this).attr('code_id')).innerHTML;
+                    $modal_code.text(code);
+                    $modal_pre.style('display', 'inline-block');
+                    Prism.highlightAll();
+                    $modal.style('display', 'block');
+                });
+            }
         }
     }
 });
